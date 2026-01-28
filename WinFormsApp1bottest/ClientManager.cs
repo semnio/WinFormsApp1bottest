cdeb9d6998deb3b582fa578d13ce8c4c2c3fe8ab
semnio <gl.semnio@yahoo.com>
2026-01-28 21:43:56 +0200

    insert class Client Manager

---
+++ WinFormsApp1bottest/ClientManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WinFormsApp1bottest
+{
+    public class ClientManager
+    {
+        // استيراد دوال الويندوز للتحكم في ذاكرة العمليات
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out int lpNumberOfBytesWritten);
+
+        public void LaunchAndPatch(string clientPath, string ipToReplace)
+        {
+            try
+            {
+                // 1. تشغيل اللعبة
+                Process sroClient = Process.Start(clientPath, "0 /22 0 0"); // بارامترات التشغيل القياسية
+
+                // 2. هنا سنضع كود البحث عن الـ IP القديم واستبداله بـ 127.0.0.1
+                // سنحتاج لعنوان الذاكرة (Offset) الخاص بالسيرفر الذي تلعب عليه
+                Console.WriteLine("تم تشغيل اللعبة وجاري حقن الـ IP...");
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("خطأ في تشغيل اللعبة: " + ex.Message);
+            }
+        }
+    }
+}
