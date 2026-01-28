6a9634e1cc690da2b14fae5fb7341a2f53694b30
semnio <gl.semnio@yahoo.com>
2026-01-28 20:23:16 +0200

    add

---
+++ WinFormsApp1bottest/PacketManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsApp1bottest
+{
+    public class PacketManager
+    {
+        // تعريف الأكواد الأساسية (Opcodes) المستخرجة من nBot/mBot
+        public static class Opcodes
+        {
+            public const ushort CLIENT_VERSION = 0x2001;
+            public const ushort CLIENT_LOGIN_REQUEST = 0x6102;
+            public const ushort CLIENT_CHARACTER_SELECTION_ACTION = 0x7001;
+            public const ushort CLIENT_MOVEMENT_REQUEST = 0x7021; // كود التحرك
+            public const ushort CLIENT_CHAT_REQUEST = 0x7025;     // كود الشات
+
+            // SERVER SIDE (الأكواد اللي بنستقبلها)
+            public const ushort SERVER_LOGIN_RESPONSE = 0xA102;
+            public const ushort SERVER_CHARACTER_LIST = 0xB007;
+        }
+
+        // دالة لتجهيز الباكت (كمثال بسيط)
+        public byte[] CreateLoginPacket(string username, string password)
+        {
+            // هنا يتم تحويل اليوزر والباسورد لبايتات (Binary) 
+            // ليتم إرسالها للسيرفر باستخدام Opcode 0x6102
+            Console.WriteLine($"تجهيز باكت الدخول للمستخدم: {username}");
+
+            // ملاحظة: التشفير (Security) سيتم إضافته هنا لاحقاً
+            return new byte[] { /* مصفوفة البيانات */ };
+        }
+
+        // دالة لمعالجة الباكتات القادمة من السيرفر
+        public void ProcessIncomingPacket(ushort opcode, byte[] data)
+        {
+            switch (opcode)
+            {
+                case Opcodes.SERVER_LOGIN_RESPONSE:
+                    // منطق نجاح أو فشل الدخول
+                    break;
+                case Opcodes.SERVER_CHARACTER_LIST:
+                    // منطق عرض الشخصيات في البوت
+                    break;
+            }
+        }
+    }
+}
